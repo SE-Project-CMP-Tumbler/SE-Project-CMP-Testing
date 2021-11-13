2b5ac80bea026b0590c550b770e1ebb31b320da5
karim taha <krm.taha@outlook.com>
2021-11-13 17:32:47 +0200

    mobile click test

--- Mobile/wdio.conf.js
+++ Mobile/wdio.conf.js
@@ -1,34 +1,40 @@
+/* eslint-disable */
 exports.config = {
-  services: ['appium'],
-  port: 4723,
+    port: 4723,
+    path: '/wd/hub/',
 
-  // PATH is important for appium local server :
-  path: '/wd/hub/',
+    runner: 'local',
+    specs: [
+        './tests/**/*.js'
+    ],
+    // Patterns to exclude.
+    exclude: [
+        // 'path/to/excluded/files'
+    ],
+    maxInstances: 10,
+    capabilities: [{
+        maxInstances: 5,
+        platformName: 'Android',
+        'appium:platformVersion': '11',
+        'appium:avd': 'Pixel',
 
-  runner: 'local',
-  specs: ['./tests/*.js'],
-  capabilities: [
-    {
-      maxInstances: 1,
-      appiumVersion: '1.22.0',
-      platformName: 'Android',
-      platformVersion: '11',
-      deviceName: 'Pixel',
-      appPackage: 'com.tumblr',
-      appActivity: 'com.tumblr.ui.activity.JumpoffActivity',
-      automationName: 'UiAutomator2'
+
+        'appium:appPackage': 'com.tumblr',
+        'appium:appActivity': 'com.tumblr.ui.activity.JumpoffActivity',
+        'appium:automationName': 'UiAutomator2',
+        'appium:udid': 'emulator-5554'
+    }],
+    logLevel: 'info',
+    bail: 0,
+    baseUrl: 'http://localhost',
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 120000,
+    connectionRetryCount: 3,
+    framework: 'mocha',
+    reporters: ['spec'],
+
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 60000
     }
-  ],
-
-  logLevel: 'trace',
-  bail: 0,
-  waitforTimeout: 10000,
-  connectionRetryTimeout: 90000,
-  connectionRetryCount: 3,
-  framework: 'mocha',
-  reporters: ['spec'],
-  mochaOpts: {
-    ui: 'bdd',
-    timeout: 60000
-  }
 }
