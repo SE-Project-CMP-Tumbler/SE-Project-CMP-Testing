48717d4a74696007fab8fda6919487e186f2218a
karim taha <krm.taha@outlook.com>
2021-11-13 17:33:21 +0200

    mobile click test

--- Mobile/tests/App.test.js
+++ Mobile/tests/App.test.js
@@ -1,5 +1,11 @@
+/* eslint-disable  */
 
 describe('Simple App testing', () => {
-  it('My first test', () => {
-  });
-});
+  it('My first test', async () => {
+    const selector = await 'new UiSelector().resourceId("com.tumblr:id/sign_up_button")'
+    const button = await $(`android=${selector}`)
+    await button.click()
+    const bg=await $(`android=${'new UiSelector().textContains("Email")'}`)
+    driver.waitUntil (()=>bg)
+  })
+})
