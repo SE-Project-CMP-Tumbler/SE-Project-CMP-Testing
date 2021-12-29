61659c3d4cc5f8de860053d0b44f2ed66f765ba7
karim taha <krm.taha@outlook.com>
2021-12-29 20:17:06 +0200

    Stubs for dashboard, post creation, post reaction

---
+++ Web/cypress/integration/Dashboard/2-reactions.spec.js
@@ -0,0 +1,53 @@
+/// <reference types="cypress" />
+
+describe('Posts: user reactions, likes, shares, reblogs, notes  ', () => {
+  it('Post exitence', () => {
+  /*
+  TODO
+  * Exists
+  * Non-empty
+  */
+  })
+  it('Tags ', () => {
+  /*
+  TODO:
+  * inspect tags
+  * try follow some
+  * check them in the user profile
+  * inspect some blogs related to this tag
+  */
+  })
+  it('Follow', () => {
+  /*
+  TODO:
+  * follow some suggested blogs, in the posts area
+  * follow certain tags
+  * check the user profile
+  */
+  })
+  it('Block, unfollow', () => {
+  /*
+  TODO:
+  * block some blogs
+  * check the user profile for the blocked users/blogs
+  * unblock, then re-check the user profile
+  * unfollow, check the user profile
+  * re-follow, .....
+  * check if you can recieve or send messages to a blocked blog
+  */
+  })
+  it('Reactions: like, comment, reblog, forward, mention, reply', () => {
+  /*
+  TODO:
+  * give a like to several posts and check the respective counts
+  * do the same for notes, adding the check for notes/comment existence
+  * optional: check for nested threads, at least 1 level nesting should work to enable a reply
+  * reblog a post, check for its exitence in the user the profile, perform this test on the rebolgged post and check the statistics
+  * forward the post to other platforms, [[do not know what to do]]
+  * mention a friend, the friend should check his notfications, check his actions if any
+  * reply to note, the user should be notified when someone replys to his note/comment
+  * copy link, I don't remember any use cases
+  */
+  })
+  it()
+})
---
+++ Web/cypress/integration/Dashboard/3-Dashbord.spec.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+
+describe('Dashboard', () => {
+  it('Infinte scroll/number of posts', () => {
+    /*
+    TODO
+    * Scroll for certain amount, eg: bottom, count the cumulative number of posts
+    * check if the loading icon appears at the bottom, if yes wait then scroll, check the number of posts has doubled, then trippled and so on ...
+    */
+  })
+  it('Suggestions: check out these blogs, radar', () => {
+    /*
+    TODO:
+    * try to follow/block/... some blogs from the radar area
+    * check them in the user profile
+    * do the same for users in the check out area
+    * do the same for check out these tags in the posts area
+    */
+  })
+  it('Do as search ', () => {
+    /*
+    TODO:
+    * Do a search, check relevance of the results
+    * Navigate through any of the results
+    */
+  })
+})
