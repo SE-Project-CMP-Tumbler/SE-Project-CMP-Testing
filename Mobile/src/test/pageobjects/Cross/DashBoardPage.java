d4540266dab1c7a84e97c38bbe67c2b17eba37a6
GhiathAjam <ghiath.ajam01@eng-st.cu.edu.eg>
2022-01-01 23:21:57 +0200

    dashboard cross page objects

--- Mobile/src/test/pageobjects/Cross/DashBoardPage.java
+++ Mobile/src/test/pageobjects/Cross/DashBoardPage.java
@@ -1,22 +1,22 @@
 package Cross;
 
 import Base.Selector;
+import Base.Selector.Identifier;
 
 public enum DashBoardPage {
-    //CreatePostButton("createPostButton"),
-    HomeButton("Home\n"+"Tab "),
-    SearchButton("Search\n"+"Tab "),
-    ActivityButton("Activity\n"+"Tab "),
-    ProfileButton("Profile\n"+"Tab "),
-    CreatePostButton("//android.widget.Button[@index='1']");
-    private final String text;
+    HomeButton(Identifier.findByText, "Tab "),
+    SearchButton(Identifier.findByText, "Search\n"+"Tab "),
+    ActivityButton(Identifier.findByText, "Activity\n"+"Tab "),
+    ProfileButton(Identifier.findByText, "Profile\n"+"Tab "),
+    CreatePostButton(Identifier.findByContDesc, "Hi");
+    private final Selector S;
 
-    DashBoardPage(String txt) {
-        text = txt;
+    DashBoardPage(Identifier f, String i) {
+        S = new Selector(f, i);
     }
 
-    public String getId() {
-        return text;
+    public Selector getId() {
+        return S;
     }
 }
 
