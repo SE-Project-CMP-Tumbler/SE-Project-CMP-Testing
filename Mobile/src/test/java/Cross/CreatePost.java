40eda8b224a2b63df123e425ab1475a8a1782faf
karim taha <krm.taha@outlook.com>
2022-01-01 23:58:41 +0200

    Create Basic text post

--- Mobile/src/test/java/Cross/CreatePost.java
+++ Mobile/src/test/java/Cross/CreatePost.java
@@ -1,13 +1,44 @@
 package Cross;
 
+import Base.Selector;
 import Base.Setup;
-import io.appium.java_client.android.AndroidElement;
-import org.openqa.selenium.By;
+import Native.CreatePostPage;
+import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
-import java.util.List;
 
-import static Cross.Utils.*;
+import static Native.Utils.DoesExist;
+import static Native.Utils.findElement;
+
 public class CreatePost extends Setup {
+    public static Selector TextPost() {
+        findElement(Cross.DashBoardPage.CreatePostButton.getId()).click();
+        String time = String.valueOf(System.nanoTime());
+        findElement(CreatePostPage.PostTextField.getId()).sendKeys(time);
+        Selector S = new Selector(Selector.Identifier.findByText, time);
+        findElement(S).click();
+        findElement(CreatePostPage.AddStyle.getId()).click();
+        findElement(CreatePostPage.AddStyle.getId()).click();
+        findElement(CreatePostPage.SubmitPost.getId()).click();
+        findElement(Cross.DashBoardPage.HomeButton.getId()).click();
+        findElement(Cross.DashBoardPage.HomeButton.getId()).click();
+        findElement(Cross.DashBoardPage.HomeButton.getId()).click();
+
+        Assert.assertTrue(DoesExist(S), "Text post failed");
+        //==================================================================
+        findElement(Cross.DashBoardPage.ProfileButton.getId()).click();
+        try {
+            Assert.assertTrue(DoesExist(S), "Text post was submitted in the dashboard, but is not found in the profile page");
+        } catch (AssertionError e) {
+            Reporter.log(e.getMessage());
+        }
+        findElement(Cross.DashBoardPage.HomeButton.getId()).click();
+        Reporter.log("The test was to submit a post and check if it appears in the dashboard," +
+                " then navigate to the user profile to see if it appears there too.");
+
+        return S;
+    }
+
     @Test
     private void logIn() {
         // Note: Can be refactored inside the Cross package, not the base package, since different one will be used for android
@@ -20,23 +51,27 @@ public class CreatePost extends Setup {
     }
 
     @Test(groups = {"Cross"})
-    public void Post() throws InterruptedException {
+    public void Post() {
         //logIn();  not necessary to be called, maybe called after logIn test
         // singUp();
-        Thread.sleep(10000);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         // click the home button
-        findElementByContentDesc(DashBoardPage.HomeButton.getId()).click();
-        //Utils.findElementByClassNameIndex(DashBoardPage.CreatePostButton.getId()).click();
+        findElement(DashBoardPage.CreatePostButton.getId()).click();
+        findElement(DashBoardPage.HomeButton.getId()).click();
+
         /*
-        * TODO:
-        *  write post
-        *  click Post
-        *  Navigate to profile tab
-        *  click posts
-        *  try to navigate to it
-        *  Check if the post appears in the dashboard
-        *
-        */
+         * TODO:
+         *  write post
+         *  click Post
+         *  Navigate to profile tab
+         *  click posts
+         *  try to navigate to it
+         *  Check if the post appears in the dashboard
+         */
     }
 
 }
