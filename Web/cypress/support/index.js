78a79e1c79e6345dc164b8ec7e4ce1ceef7c653b
GhiathAjam <ghiath.ajam01@eng-st.cu.edu.eg>
2022-01-02 02:33:45 +0200

    code coverage DONE

--- Web/cypress/support/index.js
+++ Web/cypress/support/index.js
@@ -16,6 +16,9 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
+// for code coverage report
+import '@cypress/code-coverage/support'
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
